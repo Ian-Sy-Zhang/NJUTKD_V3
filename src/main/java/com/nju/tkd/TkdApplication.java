f5b3404325cbc7bb5f685350ad1faa0b43d0899c
0APPTI0 <2458530429@qq.com>
2019-04-08 22:21:47 +0800

    建立项目，添加依赖

---
+++ src/main/java/com/nju/tkd/TkdApplication.java
@@ -0,0 +1,13 @@
+package com.nju.tkd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TkdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TkdApplication.class, args);
+    }
+
+}
